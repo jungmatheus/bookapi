65f2b0de044b2ced58eeb5399dce321e9bf613cf
jungmatheus <jungcmatheus@gmail.com>
2022-09-11 11:10:07 +0200

    removed auth tests to /books endpoint

--- app/tests/test_books.py
+++ app/tests/test_books.py
@@ -8,15 +8,6 @@ def test_get_books(authorized_client, create_books):
     assert res.status_code == 200
 
 
-def test_unauthorized_user_get_all(client, create_books):
-    res = client.get('/books/')
-    assert res.status_code == 401
-
-    
-def test_unauthorized_user_get_one(client, create_books):
-    id = create_books[0].id
-    res = client.get(f"/books/{id}")
-    assert res.status_code == 401
 
 def test_invalid_id(authorized_client, create_books):
     res = authorized_client.get('/books/9999')
